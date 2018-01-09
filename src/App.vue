b243fe98581ce94ba1b814440182bc53eb249a00
maruko <opcmaruko@gmail.com>
2018-01-09 23:21:31 +0800

    完成

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,28 @@
-<template>
-  <div id="app">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+<template lang="pug">
+  #app
+      .navbar.navbar-expand-lg.navbar-light.bg-light
+        a.navbar-brand(href="#") Vue練習
+        button.navbar-toggler(type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation")
+          span.navbar-toggler-icon
+        
+        .collapse.navbar-collapse#navbarSupportedContent
+          ul.navbar-nav.mr-auto
+            li.nav-item
+              router-link.nav-link(:to="'/'") 颱風防災資訊平台網頁前端實作
+            li.nav-item
+              router-link.nav-link(:to="'vue2'") vue基本練習
+            li.nav-item
+              router-link.nav-link(:to="'list'") vue list練習
+            
+          .col-md-1 Hi! {{ showUsername }}
+          select.form-control.col-md-2(@change='changestatus', v-model='status')
+            option(value='上線') 上線
+            option(value='下線') 下線
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <!-- <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a> -->
-              <router-link class="nav-link" :to="'/'">HelloWorld</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" :to="'vue2'">vue2</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" :to="'list'">list</router-link>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Dropdown
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Something else here</a>
-              </div>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" href="#">Disabled</a>
-            </li>
-          </ul>
-          
-        </div>
-        <div class="col-md-1">Hi! {{ showUsername }}</div>
-        <select class="form-control col-md-2" @change="changestatus" v-model="status">
-          <option value="上線">上線</option>
-          <option value="下線">下線</option>
-        </select>
-    </nav>
-   
-    <router-view @pushNewName="getNewName" ></router-view>
-  </div>
+      router-view(@pushnewname='getNewName')
+
+            
+ 
 </template>
 
 <script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,50 +1,88 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+<template lang="pug">
+  .hello.container
+    h1 {{ msg }}
+    h2 颱風防災資訊平台網頁前端實作
+
+    
+    select(name="" id="" class="form-control" required="required" v-model="area")
+      option(value="全部") 全部     
+      option(value="萬華區") 萬華區
+      option(value="中正區") 中正區
+      option(value="大同區") 大同區
+      option(value="中山區") 中山區
+      option(value="大安區") 大安區
+      option(value="南港區") 南港區
+      option(value="文山區") 文山區
+      option(value="松山區") 松山區
+      option(value="信義區") 信義區
+      option(value="士林區") 士林區
+      option(value="北投區") 北投區
+      option(value="內湖區") 內湖區
+
+    table(class="table table-striped")
+      thead
+        tr
+          th(scope="col") 發生時間
+          th(scope="col") 區域
+          th(scope="col") 詳細位置
+          th(scope="col") 描述
+      
+    
+      tbody
+        tr(v-for="(item,i) in listData" v-if="changearea(item)")
+          th(scope="row") 
+            p(class="h6") {{ item.CaseTime }}
+          th(scope="row") 
+            button(type="button" class="btn btn-outline-primary btn-sm") {{ item.CaseLocationDistrict }}
+          th(scope="row") 
+            p(class="h6") {{ item.CaseLocationDescription }}
+          th(scope="row") 
+            p(class="h6") {{ item.CaseDescription }}
+    
+    
+ 
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "hello",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      area:"全部",
+      msg: "maruko test",
+      listData: []
+    };
+  },
+  methods: {
+    changearea(item){
+      if(this.area=="全部"){
+        return true
+      }else if(item.CaseLocationDistrict==this.area){
+        return true
+      }
+    },
+    getData() {
+      let vm = this;
+      vm.axios
+        .get(
+          "https://tcgbusfs.blob.core.windows.net/blobfs/GetDisasterSummary.json"
+        )
+        .then(function(response) {
+          console.log(response);
+          vm.listData =
+            response.data.DataSet["diffgr:diffgram"].NewDataSet.CASE_SUMMARY;
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
     }
+  },
+
+  mounted() {
+    this.getData();
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+
+
